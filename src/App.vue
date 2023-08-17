19e40f21cf77c5657376672bb8cccf2209a81e5c
Ivan Melnyk <ivanmel.vn23@gmail.com>
2023-08-17 19:41:49 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <n-space>
+          <n-button type="primary" @click="$router.push('/')">Home</n-button>
+          <n-button type="warning" @click="$router.push('/about')">About</n-button>
+        </n-space>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { NButton, NSpace } from 'naive-ui'
+
+export default defineComponent({
+    components: {
+      NButton
+    }
+  })
+</script>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about">
+    <n-button type="primary">
+      Primary
+    </n-button>
+  </div>
+</template>
+
+<style></style>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { NButton } from 'naive-ui'
+
+export default defineComponent({
+    components: {
+      NButton
+    }
+  })
+</script>
