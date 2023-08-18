851be7073d565917cfa142dbbeda0dc0f5aafb7d
Ivan Melnyk <ivanmel.vn23@gmail.com>
2023-08-18 13:07:27 +0300

    Config update

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,9 +7,9 @@ module.exports = {
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
+    'plugin:json/recommended',
   ],
-  'parser': '@babel/eslint-parser',
   parserOptions: {
     ecmaVersion: 'latest'
   },
