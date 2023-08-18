b1dfcf5aae32e0c806ccf1bc7374cb3e3cafac31
Ivan Melnyk <ivanmel.vn23@gmail.com>
2023-08-18 13:07:51 +0300

    Header created

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { NLayoutHeader, NAvatar, NButton, NSpace } from 'naive-ui';
+
+let inverted = ref<boolean>();
+</script>
+
+<template>
+    <n-layout-header class="header padding-small" :bordered="inverted">
+        <n-space align="center">
+            <!-- TODO: change logo to vector image -->
+            <n-avatar round :size="64" src="src/assets/img/osnova-logo.jpg" />
+            <n-button type="primary" @click="inverted = !inverted">Switch</n-button>
+        </n-space>
+    </n-layout-header>
+</template>
+
+<style>
+.header {
+    color: white;
+}
+
+n-avatar {
+    padding: 0;
+}
+
+.padding-small {
+    padding: 4px;
+}
+</style>
