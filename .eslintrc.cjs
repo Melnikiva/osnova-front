d426a509cb3a5e4316b2c6dc10064b9f6fd9879d
Ivan Melnyk <ivanmel.vn23@gmail.com>
2023-08-19 23:17:22 +0300

    Eslint config update

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,17 @@
 /* eslint-disable no-undef */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier/skip-formatting',
     'plugin:json/recommended',
   ],
-  'parser': 'vue-eslint-parser',
+  parser: 'vue-eslint-parser',
   parserOptions: {
-    ecmaVersion: 'latest'
+    ecmaVersion: 'latest',
   },
-}
+};
